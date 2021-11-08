c73970ee070ed339bf434069bdc0c42f42144e08
Jovinac Andrea <contact@jovinacandrea.fr>
2021-11-08 22:44:31 +0100

    Step 3 : Avancement de la Home
    CSS, de la home, ajout de la content et du formulaire

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,27 @@
 import React from "react";
 import Header from "../Composants/Header";
+import "../Styles/Home.css";
+import Img from "../Images/netflix-image.jpeg";
 
 function Home() {
   return (
     <div className="home">
       <Header />
+      <section className="section">
+        <div className="overlay">
+          <div className="content-section">
+            <form>
+              <select id="catagorie" name="categorie">
+                <option value="action">Action</option>
+                <option value="animation">Animation</option>
+                <option value="horreur">Horreur</option>
+                <option value="drame">Drame</option>
+              </select>
+              <input placeholder="Tapez votre mot de recherche"></input>
+            </form>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
